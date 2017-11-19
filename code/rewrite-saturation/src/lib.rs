143668635da4a93a3fab1e220cf0f8e214dbc067
Alexander Ronald Altman <alexanderaltman@me.com>
2017-11-19 09:58:34 -0600

    Write more code.

--- code/rewrite-saturation/src/lib.rs
+++ code/rewrite-saturation/src/lib.rs
@@ -1,2 +1,9 @@
 extern crate itertools;
+extern crate petgraph;
+#[macro_use] extern crate quickcheck;
+#[macro_use] extern crate quickcheck_derive;
 use itertools::{Itertools, PeekingNext};
+use petgraph::prelude::*;
+use quickcheck::Arbitrary;
+
+pub mod rewriting_system;

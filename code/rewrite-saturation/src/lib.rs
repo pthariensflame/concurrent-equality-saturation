e24552fcd7d63a39d04f502d5305f974ebc35772
Alexander Ronald Altman <alexanderaltman@me.com>
2017-11-19 14:03:43 -0600

    Separate dev dependencies.

--- code/rewrite-saturation/src/lib.rs
+++ code/rewrite-saturation/src/lib.rs
@@ -1,9 +1,6 @@
 extern crate itertools;
 extern crate petgraph;
-#[macro_use] extern crate quickcheck;
-#[macro_use] extern crate quickcheck_derive;
 use itertools::{Either, Itertools, PeekingNext};
 use petgraph::prelude::*;
-use quickcheck::Arbitrary;
 
 pub mod rewriting_system;
